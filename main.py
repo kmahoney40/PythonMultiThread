2d1c801965fb5e452962b3432a9c13dccdd10506
Kevin Mahoney <kmahoney40@hotmail.com>
2018-02-12 05:02:30 +0000

    added do_every.py

--- main.py
+++ main.py
@@ -48,14 +48,49 @@ kmworldCount = 0
 counters = [0, 0, 0]
 cmnd = ord(' ')
 readChar = " "
-
+retVal = [""]
 if __name__ == '__main__':
 
     import time
+    import datetime
 
     scr = curses.initscr()
     curses.noecho()
     scr.nodelay(1)
+
+    def do_every(period, fn, *args):
+        def g_tick():
+            t = time.time()
+            count = 0
+            while True:
+                count += 1
+                yield max(t + count * period - time.time(), 0)
+
+        g = g_tick()
+        while True:
+            time.sleep(g.next())
+            fn(*args)
+
+# need to do a combination of these 2 things. Create a thread manager and use the 
+# do_every method as the timer. Incorporate into the therad manager.
+
+
+
+    def hello1(s):
+        scr = curses.initscr()
+        curses.noecho()
+        scr.nodelay(1)
+        scr.addstr(s[1], 0, "hello1 {} ({:.4f})".format(s[0], time.time()))
+        time.sleep(2)
+
+    def hello2(s):
+        src = curses.initscr()
+        curses.noecho()
+        scr.nodelay(1)
+        scr.addstr(s[1], 0, "hello2 {} ({:.4f})".format(s[0], time.time()))
+    
+    do_every(10, hello1, ["woot", 5])
+    do_every(5, hello2, ["5 Sec", 10])
 
     objCfg = readConfig.ConfigObj("config.txt") 
     if objCfg.exception != None:
@@ -73,31 +108,56 @@ if __name__ == '__main__':
         count[idx] += 1
 
     readServer = readFromServer.ReadFromServer()
-    retVal = ""
+    scr.addstr(17, 0, readServer.name)
+
+    scr.addstr(18, 0, str(counters[0]) + " "+ str(counters[1]))
+
     timer = Manager()
     timer.add_operation(hello, 5, [counters, 0])
-    timer.add_operation(kmworld, 3, [counters, 1])
-    timer.add_operation(readServer.readFromServer, 10, [objCfg, retVal])
+    #timer.add_operation(kmworld, 3, [counters, 1])
+    #timer.add_operation(readServer.readFromServer, 10, [objCfg, retVal])
 
-    scr.addstr(14, 0,retVal)
+    scr.addstr(19, 0, retVal[0] + "WOOT")
 
     myContinue = True
     keepGoing = True
-    while keepGoing:
+    startTimer = [0, False, False]
+
+    timer.add_operation(readServer.readFromServer, 10, [objCfg, retVal])
+
+    while False:# keepGoing:
         scr.addstr(0, 0, "Press \"h\" for Help and \"q\" to exit...")
+
+        dtm = datetime.datetime.now()
+        scr.addstr(21, 0, "time: " +  str(dtm) + " min: " + str(dtm.minute) + " sec: " + str(dtm.second) + " ")
+        if(startTimer[0] == 0):
+            ms = dtm.microsecond
+            waitMs = 1.0 - (ms / 1000000.0)
+            scr.addstr(22, 0, "time: " +  str(dtm.microsecond) + " " +  str(dtm.second))
+            time.sleep(waitMs)
+#            timer.add_operation(readServer.readFromServer, 10, [objCfg, retVal])
+            startTimer[0] = 1
+
+        scr.addstr(25, 0, "startTimer[0]: " + str(startTimer[0]))
+        if(int(dtm.minute) % 3 == 0 and int(dtm.second) == 0 and startTimer[0] == 1):
+            timer.add_operation(kmworld, 180, [counters, 1])
+            startTimer[0] = 2
+
+        scr.addstr(23, 0, "time: " +  str(dtm.microsecond) + " " +  str(dtm.second))
+
 
         readChar = scr.getch()
         if(readChar == curses.ERR):
             readChar = ord(' ')
-        #lst = [readChar]
-        #readChar = "".join(chr(i) for i in lst)
         cmnd = readChar
         if readChar != ord(' '):
             counters[2] = readChar
         scr.addstr(5, 0, str(readChar) + "  ")
         if(readChar == ord('q')):
             keepGoing = False
-        #line += 1
+        
+        scr.addstr(20, 0, "retVal[0] " + retVal[0])
+        
         time.sleep(1)
 
     curses.endwin()
--- readFromServer.py
+++ readFromServer.py
@@ -1,22 +1,28 @@
 import requests
 import sys
 import curses
+import time
 
 class ReadFromServer:
     scr = curses.initscr()
     curses.noecho()
     scr.nodelay(1)
 
+    counter = 0
+
     def __init__(self):
         self.name = "ReadFromServerInstance"
 
     def readFromServer(self, cfgObj, ret):
         try:
-#            scr.addstr(25, 0, "ReadFromServer")
+            #scr.addstr(26, 0, "ReadFromServer")
             url = cfgObj.url + "door"
-            ret = requests.get(url)
-#            scr.addstr(20, 0, str(ret))
-            raise ValueError(str(ret))
+            ret[0] = "ASDF: " + url + str(time.time()) + " " + str(requests.get(url).text)
+            #counter += 1
+            #scr.addstr(27, 0, str(ret[0]))
+            #myError = 1/0
+           # raise ValueError(str(ret))
+            time.sleep(2)
         except Exception, ex:
-            sys.exit("Error in readFromServer! " + str(ex))
+            sys.exit("Error in readFromServer! " + str(ex.message))
 
